0ea9e5ce618f28812fcaeee6ff69ecd49efef7df
TechnoStudy <ismettechno@gmail.com>
2023-03-09 17:40:59 +0000

    Gun02 Assert

--- src/Gun02/_02_Ornek.java
+++ src/Gun02/_02_Ornek.java
@@ -1,14 +1,32 @@
 package Gun02;
 
 import Utlity.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class _02_Ornek extends BaseDriver {
 
     @Test
-    void test1()
+    void loginTest()
     {
-        System.out.println("test 1 çalıştı");
+        System.out.println("Login Test");
+
+        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
+
+        WebElement inputEmail=driver.findElement(By.id("input-email"));
+        inputEmail.sendKeys("testng1@gmail.com");
+
+        WebElement password=driver.findElement(By.id("input-password"));
+        password.sendKeys("123qweasd");
+
+        WebElement loginBtn=driver.findElement(By.xpath("//input[@type='submit']"));
+        loginBtn.click();
+
+        Assert.assertTrue(driver.getTitle().equals("My Account"));
+        //Assert.assertEquals(driver.getTitle(),"My Account", "Login olamadı");
+        //Assert.assertTrue(driver.getCurrentUrl().contains("account/account"));
     }
 
 
