cdb3d432b9e7454121db424b6c99fffc67db8ea5
TechnoStudy <ismettechno@gmail.com>
2023-03-14 18:52:09 +0000

    Gun04

--- src/Gun04/_04_ContactUs.java
+++ src/Gun04/_04_ContactUs.java
@@ -6,5 +6,29 @@ package Gun04;
   3- Submit ettikten sonra Contact US yazısını doğrulayınız.
  */
 
-public class _04_ContactUs {
+import Utlity.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class _04_ContactUs extends BaseDriver {
+
+    @Test
+    @Parameters("mesaj")// xml deki parametre adı aynı olmak zorunda
+    void contactUs(String gelenMesaj){
+        // String gelenMesaj:metod için gerekli tanımlama aynı olmak zorunda değil
+
+        WebElement contactUsBtn= driver.findElement(By.linkText("Contact Us"));
+        contactUsBtn.click();
+
+        WebElement enquiryArea= driver.findElement(By.id("input-enquiry"));
+        enquiryArea.sendKeys(gelenMesaj);
+
+        WebElement submitButton= driver.findElement(By.cssSelector("[type='submit']"));
+        submitButton.click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("success"));
+    }
 }
