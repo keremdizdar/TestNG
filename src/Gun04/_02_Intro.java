f003007233d3750e2b415eb7e7da3113dd428690
TechnoStudy <ismettechno@gmail.com>
2023-03-14 17:46:41 +0000

    Gun04 contactus

--- src/Gun04/_02_Intro.java
+++ src/Gun04/_02_Intro.java
@@ -1,4 +1,12 @@
 package Gun04;
 
+import org.testng.annotations.Test;
+
 public class _02_Intro {
+
+    @Test
+    void Intro2_Test1() { System.out.println("Intro_2 -> Test 1"); }
+
+    @Test
+    void Intro2_Test2() { System.out.println("Intro_2 -> Test 2"); }
 }
---
+++ src/Gun04/_04_ContactUs.java
@@ -0,0 +1,10 @@
+package Gun04;
+
+/*
+  1- ContactUs a tıklayınız
+  2- Mesaj kutusuna en az 10 karakterlik bir mesaj yazınız.
+  3- Submit ettikten sonra Contact US yazısını doğrulayınız.
+ */
+
+public class _04_ContactUs {
+}
