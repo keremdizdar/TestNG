06ddc66f8304a521be8ea2f9b0261264a875c0d5
TechnoStudy <ismettechno@gmail.com>
2023-03-15 16:48:02 +0000

    Gun05 cross browser switch

--- src/Utlity/BaseDriverParameter.java
+++ src/Utlity/BaseDriverParameter.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -23,48 +25,55 @@ public class BaseDriverParameter {
 
     @BeforeClass
     @Parameters("browserTipi")
-    public void baslangicIslemler(String browserTipi)
-    {
-        Logger logger= Logger.getLogger("");
+    public void baslangicIslemler(String browserTipi) {
+        Logger logger = Logger.getLogger("");
         logger.setLevel(Level.SEVERE);
 
-        if (browserTipi.equalsIgnoreCase("firefox")) {
-            System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
-            driver = new FirefoxDriver();
-            System.out.println("firefox started");
-        }
-        else
+        switch (browserTipi.toLowerCase())
         {
-            System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            driver = new ChromeDriver(options);
-        }
+            case "firefox" :
+                System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
+                driver = new FirefoxDriver();
+                System.out.println("firefox started");
+                break;
 
+            case "safari":
+                driver=new SafariDriver();
+                break;
+
+            case "edge":
+                driver=new EdgeDriver();
+                break;
+
+            default:
+                System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                driver = new ChromeDriver(options);
+        }
 
         //driver.manage().window().maximize(); // Ekranı max yapıyor.
-        Duration dr=Duration.ofSeconds(30);
+        Duration dr = Duration.ofSeconds(30);
         driver.manage().timeouts().pageLoadTimeout(dr);
         driver.manage().timeouts().implicitlyWait(dr);
 
-        wait=new WebDriverWait(driver,
+        wait = new WebDriverWait(driver,
                 Duration.ofSeconds(30));
         loginTest();
     }
 
-    void loginTest()
-    {
+    void loginTest() {
         System.out.println("Login Test");
 
         driver.get("https://opencart.abstracta.us/index.php?route=account/login");
 
-        WebElement inputEmail=driver.findElement(By.id("input-email"));
+        WebElement inputEmail = driver.findElement(By.id("input-email"));
         inputEmail.sendKeys("testng1@gmail.com");
 
-        WebElement password=driver.findElement(By.id("input-password"));
+        WebElement password = driver.findElement(By.id("input-password"));
         password.sendKeys("123qweasd");
 
-        WebElement loginBtn=driver.findElement(By.xpath("//input[@type='submit']"));
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
         loginBtn.click();
 
         Assert.assertTrue(driver.getTitle().equals("My Account"));
@@ -73,8 +82,7 @@ public class BaseDriverParameter {
     }
 
     @AfterClass
-    public void bitisIslemleri()
-    {
+    public void bitisIslemleri() {
         Tools.Bekle(5);
         driver.quit();
     }
