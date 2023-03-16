4d985611110f2c85cb4925d8de4e7f27a6ed6677
TechnoStudy <ismettechno@gmail.com>
2023-03-16 18:53:25 +0000

    Gun06 ek

---
+++ src/Gun07/_01_WishList.java
@@ -0,0 +1,19 @@
+package Gun07;
+
+/*
+    Senaryo ;
+    1- Siteye gidiniz..
+
+   1.Test;
+   2- "ipod" ürününü aratınız."ipod" u xml den gönderiniz
+   3- çıkan elamnlardan random bir elelamnı Add Wish butonuna tıklayınız.
+   4- Daha sonra WishList e tıklatınız
+   5- Burada çıkan ürünle tıklanan ürünün isminin aynı olup olmadığını doğrulayınız.
+ */
+
+import Utlity.BaseDriver;
+
+public class _01_WishList extends BaseDriver {
+
+
+}
