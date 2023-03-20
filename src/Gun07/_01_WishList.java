94074db9e97eadbccd3796817e484e004c754d37
TechnoStudy <ismettechno@gmail.com>
2023-03-20 16:39:28 +0000

    Gun07 - 1

--- src/Gun07/_01_WishList.java
+++ src/Gun07/_01_WishList.java
@@ -12,8 +12,44 @@ package Gun07;
  */
 
 import Utlity.BaseDriver;
+import Utlity.Tools;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class _01_WishList extends BaseDriver {
+
+
+   @Test
+   @Parameters("searchText")
+   void addToWishList(String text){
+
+      WebElement searchInput= driver.findElement(By.cssSelector("[name='search']"));
+      searchInput.sendKeys(text);
+
+      WebElement searchButton= driver.findElement(By.cssSelector("[class='btn btn-default btn-lg']"));
+      searchButton.click();
+
+      List<WebElement> searhResult=driver.findElements(By.cssSelector("div[class='caption']>h4>a"));
+      int randomSecim= Tools.RandomGenerator(searhResult.size()); // 0,1,2,3
+      String wishItemText=searhResult.get(randomSecim).getText(); // tıklatılacak elemanın ismini aldım
+      System.out.println("wishItemText = " + wishItemText);
+
+      List<WebElement> wishBtnList=driver.findElements(By.xpath("//button[@data-original-title='Add to Wish List']"));
+      wishBtnList.get(randomSecim).click();  // random elementin wish listine tıklattım
+
+      WebElement btnWisthProducts=driver.findElement(By.id("wishlist-total"));
+      btnWisthProducts.click();
+
+      List<WebElement> wishTableNames = driver.findElements(By.cssSelector("[class='text-left']>a"));
+
+      // wishItemText  i  wishTableNames  ın gettext lerinde varmı diye kontrol edeceğim.
+      Tools.listContainsString(wishTableNames, wishItemText);
+
+   }
 
 
 }
--- src/Utlity/Tools.java
+++ src/Utlity/Tools.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
+import java.util.List;
+
 public class Tools {
 
     public static void Bekle(int sn)
@@ -18,6 +20,26 @@ public class Tools {
     public static void successMessageValidation(){
         WebElement msgLabel= BaseDriver.driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
         Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
+    }
+
+    public static int RandomGenerator(int max){ //4
+        return (int)(Math.random()*max); // 0,1,2,3
+    }
+
+    public static void listContainsString(List<WebElement> list, String aranacakKelime){
+
+        boolean bulundu=false;
+        for(WebElement e: list){
+            if (e.getText().toLowerCase().contains(aranacakKelime.toLowerCase()))
+            {
+               bulundu=true;
+               break;
+            }
+        }
+
+        if (bulundu==false)
+            Assert.fail();
+
     }
 
 }
